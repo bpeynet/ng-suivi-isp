8d5d82b450bd07228a099585f239c1b7228955bc
Angular CLI <angular-cli@angular.io>
2018-03-03 08:28:54 +0100

    chore: initial commit from @angular/cli

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { RoutingModule } from './routing.module';
+
+import { AppComponent } from './app.component';
+
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
