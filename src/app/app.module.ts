16fa764148d469c7701e4946542767fcbef49aa3
Benoît Peynet <benoit.peynet@cgi.com>
2018-03-03 09:01:33 +0100

    Ajout d'Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { RoutingModule } from './routing.module';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 
@@ -15,7 +17,9 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     RoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
